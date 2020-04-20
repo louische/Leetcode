7a9ed17847cb2ebf3e4f1943c2654f0c109cc874
louische <32444752+louische@users.noreply.github.com>
2020-04-19 22:32:56 -0400

    Update KMP-Implementation.py

--- KMP-Implementation.py
+++ KMP-Implementation.py
@@ -3,38 +3,28 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if len(needle) == 0: return 0
         if len(haystack) == 0: return -1
-        def help(needle: str) -> []:
-            res = [0] * len(needle)
-            j = 0
-            i = 1
-            while(i < len(needle)):
-                if(needle[i] == needle[j]):
-                    res[i] = j + 1
-                    j += 1
-                else:
-                    while(j > 0 and needle[i] != needle[j]):
-                        j = res[j - 1] 
-                        if needle[i] == needle[j]:
-                            res[i] = j + 1
-                            j += 1
-                            break
-                i += 1  
-            return res
+    
+        res = [0] * len(needle)
+        j = -1
+        i = 0
+        res[0] = -1
+        while(i < len(needle)):
+            while(j >= 0 and needle[i] != needle[j]):
+                j = res[j]
+            i += 1
+            j += 1
+            res[i] = j
 
-        arr = help(needle)
-        print(arr)
         i = 0
         j = 0
-        while i < len(haystack):
-            if haystack[i] != needle[j]:
-                if j > 0:
-                    j = arr[j - 1]
-                else:
-                    i += 1
-            else:
-                i += 1
-                j += 1
+        while(i < len(haystack)):
+            while(j >= 0 and needle[j] != haystack[i]):
+                j = res[j]
+            i += 1
+            j += 1
             if j == len(needle):
-                return i - j 
-        return  -1
+                return i - len(needle)
+        return -1
+    
+
 
